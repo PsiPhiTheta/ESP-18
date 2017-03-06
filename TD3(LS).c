df6f97fa29c4219952925bd4fc838b5ffec5cab9
PsiPhiTheta <thomashollis1@gmail.com>
2017-03-06 21:09:16 +0000

    Tech Demo 3: Line Sensor (v3.1)

--- TD3(LS).c
+++ TD3(LS).c
@@ -2,20 +2,23 @@
     //Authors: Thomas Hollis, Charles Shelbourne
     //Project: ESP-18
     //Year: 2017
-    //Version: 2.1
+    //Version: 3.1
 
 //1. File inclusions required
-    #include "xc_configuration_bits.h"
+    #include "xc_config_settings.h"
     #include "plib/adc.h"
     #include "plib/timers.h"
     #include "plib/delays.h"
     #include "math.h"
     #include "plib/pwm.h"
+    #include "timers.h"
+    #include "capture.h"
 
 //2. Function prototypes
     //2a. Configuration functions
         void config_PWM(void);
         void config_LS(void);
+        void config_PS(void);
 
     //2b. Motor functions
         void move(int angle, int forward);
@@ -30,7 +33,8 @@
         unsigned char LEDarray_breakdetected(void);
 
     //2d. Proximity sensor functions
-        //none required yet
+        void interrupt isr(void);
+        void enable_global_interrupts(void);
 
     //2e. Speed encoder functions
         //none required yet
@@ -38,6 +42,9 @@
 //3. Global variables
     int x = 0;
     int time360 = 2000;
+
+    volatile char y=0,s=0;
+    volatile unsigned int logic_high =0;
 
 //4. Main Line Code (MLC)
     int main(void)
@@ -48,6 +55,9 @@
             Delay10KTCYx(250);
             LEDarray_off();
             Delay10KTCYx(250);
+
+            config_PS();
+            enable_global_interrupts();
 
             while (LEDarray_breakdetected() == 0)
             {
@@ -102,10 +112,19 @@
                 OpenPWM5(252);
             }
         void config_LS(void)
-            {    
+            {
                 ADCON1 = 0x00;
                 TRISA = 0b00000000;
             }
+        void config_PS(void)
+        {
+            OpenTimer3(TIMER_INT_OFF & T1_16BIT_RW & T1_SOURCE_INT & T1_PS_1_1 & T1_OSC1EN_ON & T1_SYNC_EXT_OFF);
+            OpenTimer0(TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_1);
+            WriteTimer0(12536);
+            OpenCapture3(CAPTURE_INT_ON & C3_EVERY_RISE_EDGE);
+   
+            TRISB =0x00;
+        }
 
     //2b. Motor functions
         void move(int angle, int forward)
@@ -198,7 +217,7 @@
                 unsigned char LS_array = 0;
                 int value = 0;
                 int checking = 0;
-                
+
                 for(int i = 0; i < 6; i++)
                 {
                     value = 0;
@@ -218,16 +237,16 @@
                         break;
                         default:break;
                     }
-                    
+
                     ConvertADC();
                     while(BusyADC());
                     value = ReadADC();
-                    
+
                     if(value > 700)
                         LS_val[i] = 1;
                     else
                         LS_val[i] = 0;
-                    
+
                     checking = LS_val [i];
                     LS_array += LS_val[i]*pow(2,i);
                 }
@@ -243,12 +262,52 @@
             {
                 unsigned char breakdetected = 0;
                 //Tom to Charlie: write a function that detects a small drop in voltage (comparator may be needed, I may join in on this one too - we might also bullshit our way through it)
+                //Line break ignored since too small to impact TCRT reading
                 breakdetected = 1;
                 return breakdetected;
             }
 
     //2d. Proximity sensor functions
-        //none required yet
+        void enable_global_interrupts(void)
+        {
+            INTCONbits.GIE =1;
+            INTCONbits.PEIE =1;
+        }
+        void interrupt isr(void)
+        {
+            if(INTCONbits.TMR0IF == 1){    //Proximity trigger signal
+                INTCONbits.TMR0IF =0;
+                s = s^1;
+                LATBbits.LATB0 =s;   //J2 13
+               WriteTimer0(40563);
+
+            }
+            if(PIR3bits.CCP3IF == 1){
+                PIR3bits.CCP3IF =0;  //CCP4 interrupt bit zeroed
+                y= y^1;              //switched between 1 and 0
+                if(y==1){
+                CCP3CON = 4;  //configure CCP 4 to interrupt on falling edge
+                WriteTimer3(0);  //refresh timer3
+
+                }
+                else
+                {
+                    logic_high = ReadTimer3();
+                    CCP3CON = 5;     //configure to interrupt on rising edge
+                    if(logic_high<1360)
+                    {    //1360 ticks of clock before LED's turn on relates to 544uS that echo signal is high => aprox 11.3cm
+                        LATA=0xFF;     // uS/48 = distance
+                    }
+                    else
+                    {
+                        LATA=0x00;
+                    }
+                }
+
+                }
+        }
 
     //2e. Speed encoder functions
         //none required yet
+
+
